a9613c7cc96d5475d7dc94e1364a3657efc28f5c
panicbit <panicbit.dev@gmail.com>
2016-05-21 13:45:51 +0200

    Init

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+/// Removing this very line makes it render correctly.
+///
+/// # I will not be rendered correctly
+///
+/// ```
+///# fn foo() {} // A hidden line like this needs to exist
+/// ```
+pub fn some_item() {}
